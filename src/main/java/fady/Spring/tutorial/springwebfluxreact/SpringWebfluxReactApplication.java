902236191fa1cbffc822e092f3b8051e4a58d401
fadyramsis <fadyramsis0@gmail.com>
2021-05-20 11:59:51 +0200

    add cricleCi

---
+++ src/main/java/fady/Spring/tutorial/springwebfluxreact/SpringWebfluxReactApplication.java
@@ -0,0 +1,13 @@
+package fady.Spring.tutorial.springwebfluxreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fady/Spring/tutorial/springwebfluxreact/SpringWebfluxReactApplicationTests.java
@@ -0,0 +1,13 @@
+package fady.Spring.tutorial.springwebfluxreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
